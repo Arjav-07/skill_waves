f0a999f1183df3e5f2e1dde1285bb438522b7dc9
Yagnajitsinh Sarvaiya-09 <yagnajeetsinh09@gmail.com>
2025-09-01 13:33:09 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,21 @@
 import 'package:flutter/material.dart';
-import 'package:skill_waves/pages/forgotpassword.dart';
-import 'package:skill_waves/pages/login_page.dart';
-import 'package:skill_waves/pages/signup.dart';
-import 'package:skill_waves/pages/home.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+
+import 'package:skill_waves/pages/ForgotPasswordPage.dart';
+import 'package:skill_waves/pages/LoginPage.dart';
+import 'package:skill_waves/pages/SignupPage.dart';
+import 'package:skill_waves/pages/home_page.dart'; // ✅ Import HomePage
 import 'package:skill_waves/utils/routes.dart';
 import 'package:skill_waves/widget/themes.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   runApp(const MyApp());
 }
 
@@ -26,8 +35,7 @@ class MyApp extends StatelessWidget {
         MyRoutes.loginRoute: (context) => const LoginPage(),
         MyRoutes.SignUpPageRoute: (context) => const SignupPage(),
         MyRoutes.forgotPasswordRoute: (context) => const ForgotPasswordPage(),
-        MyRoutes.homeRoute: (context) => const HomePage(),
-        // Add other routes here if needed
+        MyRoutes.homeRoute: (context) => const HomePage(), // ✅ Added
       },
     );
   }
